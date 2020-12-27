32f1d7124410d85abb3d7f4a77fe09ed879ebc7b
gururaj3 <guruz.shinde@gmail.com>
2020-12-27 16:13:43 -0500

    added Implement Stack Using Queues

---
+++ Implement Stack Using Queues/TwoQueues.java
@@ -0,0 +1,45 @@
+class MyStack {
+
+    /** Initialize your data structure here. */
+    Queue<Integer> q;
+    Queue<Integer> temp;
+    public MyStack() {
+        this.q = new LinkedList<>();
+        this.temp = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        while(!q.isEmpty()){
+            temp.add(q.poll());
+        }
+        q.add(x);
+        while(!temp.isEmpty()){
+            q.add(temp.poll());
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return q.poll();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return q.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
