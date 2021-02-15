61c9ac766b75b6ce6f350424f5cea4ea0bdb9b50
gururaj3 <guruz.shinde@gmail.com>
2021-02-14 23:41:04 -0500

    added Max Consecutive Ones

---
+++ Max Consecutive Ones/MySol.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+        int count = 0, max = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            
+            if(nums[i] == 1){
+                count++;
+            }
+            else{
+                max = Math.max(count, max);
+                count = 0;
+            }
+            
+        }
+        return Math.max(count, max);
+    }
+}
+
+// count = 0
+    
+    
+// Loop over array:
+//     if(1)
+//         count++
+//     else:
+//         count = 0
+//         Max(count, min);
+
+// return max(count, )
