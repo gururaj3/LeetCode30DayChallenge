71ac89f710cc5d775694bf14b787f60015cda834
gururaj3 <guruz.shinde@gmail.com>
2021-02-23 23:20:21 -0500

    updated Number of Days Between Two Dates

--- Number of Days Between Two Dates/DifferenceFromStartYear.java
+++ Number of Days Between Two Dates/DifferenceFromStartYear.java
@@ -1,3 +1,5 @@
+// Refer - https://www.geeksforgeeks.org/find-number-of-days-between-two-given-dates/
+
 class Solution {
     public int daysBetweenDates(String date1, String date2) {
 		return Math.abs(getDaysFrom19710101(date1) - getDaysFrom19710101(date2));
@@ -9,7 +11,7 @@ class Solution {
 		int month = Integer.valueOf(str[1]);
 		int day = Integer.valueOf(str[2]);
         
-        int count = year * 365 + day;
+        int count = year * 365 + day;     //Either consider it starting from 0000/00/00/ or as given in the constraints 1971/00/00 -> so it could be year - 1971 and so on calculate 
 		
 		int monthDays[] = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
