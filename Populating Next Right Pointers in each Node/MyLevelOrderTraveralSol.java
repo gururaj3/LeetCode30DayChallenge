e4f954e2ef0ceef0bf2680a00d4ec3f8c6180759
gururaj3 <guruz.shinde@gmail.com>
2021-03-01 13:45:43 -0500

    added Populating Next Roght Pointers in each Node

---
+++ Populating Next Right Pointers in each Node/MyLevelOrderTraveralSol.java
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        
+        if(root == null)
+            return null;
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            
+            int len = q.size();
+            List<Node> list = new ArrayList<>();
+            for(int i=0; i<len; i++){
+                Node temp = q.poll();
+                list.add(temp);
+                if(temp.left != null)
+                    q.add(temp.left);
+                if(temp.right != null)
+                    q.add(temp.right);
+            }
+
+            for(int i=0; i<len-1; i++){
+                list.get(i).next = list.get(i+1);
+            }
+            list.get(len-1).next = null;
+            
+            
+        }
+        return root;
+    }
+} 
