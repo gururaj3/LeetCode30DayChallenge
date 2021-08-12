1ee93e493147be967906d999ad1058ab0e47fa23
gururaj3 <guruz.shinde@gmail.com>
2021-08-12 00:34:58 -0700

    updated Subarray Sum equals K

--- Subarray Sum equals K/CumulativeSumHashMap.java
+++ Subarray Sum equals K/CumulativeSumHashMap.java
@@ -22,3 +22,35 @@ Prefix Sum = [3,7,14,16,13,14,18,20,21]
 Array = [3,4,7,2,-3,1,4,2,1]
 Sub arrays = [3,4][7][7,2,-3,1],[1,4,2][2,-3,1,4,2,1][4,2,1]
 */
+
+/*
+Solution without initializing (0,1) in the map:
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, currSum = 0;
+        HashMap<Integer, Integer> h = new HashMap();
+        
+        for (int num : nums) {
+            // current prefix sum
+            currSum += num;
+            
+            // situation 1:  
+            // continuous subarray starts 
+            // from the beginning of the array
+            if (currSum == k)
+                count++;
+            
+            // situation 2:
+            // number of times the curr_sum − k has occured already, 
+            // determines the number of times a subarray with sum k 
+            // has occured upto the current index
+            count += h.getOrDefault(currSum - k, 0);
+            
+            // add the current sum
+            h.put(currSum, h.getOrDefault(currSum, 0) + 1);    
+        }
+                
+        return count;
+    }
+}
+*/
